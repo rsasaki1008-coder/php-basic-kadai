0dc4a2c78cdd654c55a00b44c2e5a753ca8b6d35
rsasaki1008-coder <rsasaki1008@gmail.com>
2026-01-30 09:53:48 +0900

    PHP5章課題提出

---
+++ kadai_005/score.php
@@ -0,0 +1,22 @@
+<?php
+// 変数に点数を代入
+$score1 = 80;
+$score2 = 60;
+$score3 = 55;
+$score4 = 40;
+$score5 = 100;
+$score6 = 25;
+$score7 = 80;
+$score8 = 95;
+$score9 = 30;
+$score10 = 60;
+
+// 合計点を算出（四則演算）
+$total = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+
+// 平均点を算出（合計点 / 10人）
+$average = $total / 10;
+
+// ブラウザに平均点を出力
+echo $average;
+?>
